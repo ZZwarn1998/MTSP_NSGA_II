4d47a3bc3a5956dffdbd4169033c37f52e7d8999
ansonlcy <1049871217@qq.com>
2021-12-17 13:29:35 +0800

    change

--- MTSP_NSGA_II_V3/code/repeat_test.py
+++ MTSP_NSGA_II_V3/code/repeat_test.py
@@ -2,18 +2,23 @@ from ga import GA
 import json
 
 if __name__ == "__main__":
+    problem_name_list = ["mtsp51", "mtsp100", "mtsp150", "pr76", "pr152", "pr226"]
+    num_travellers = 5
+    population_size = 100
+    generations = 200
+    mutation_rate = 0.2
+    repeat_times = 30
 
     record_dict = {}
-
-    for data_name in ["mtsp51", "mtsp100", "mtsp150", "pr76", "pr152", "pr226"]:
+    for data_name in problem_name_list:
         tmp_dict = {}
         run_time_all = []
         optimal_length_all = []
         optimal_path_all = []
         minfun1val_per_round_all = []
 
-        for i in range(30):
-            ga = GA(data_name, 5, 100, 200, 0.2)
+        for i in range(repeat_times):
+            ga = GA(data_name, num_travellers, population_size, generations, mutation_rate)
             runtime, optimal_length, optimal_path, minfun1val_per_round = ga.solve()
             run_time_all.append(runtime)
             optimal_length_all.append(optimal_length)
